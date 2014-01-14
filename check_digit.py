f7ddb810d0fe1a3d25900e0a4733a9fa4db97869
matt-boyd <admin@matt-boyd.co.uk>
2014-01-14 13:06:00 +0000

    check_digit.py - Main code.
    Simple python that does not require any extra modules.

---
+++ check_digit.py
@@ -0,0 +1,19 @@
+one = input()
+two = input()
+three = input()
+four = input()
+five = input()
+six = input()
+seven = input()
+eight = input()
+nine = input()
+ten = input()
+
+total = (one * 2) + (two * 3) + (three * 4) + (four * 5) + (five * 6) + (six * 7) + (eight * 9) + (nine * 10) + (ten * 11)
+
+remainder = total % 11
+
+answer = 11 - remainder
+
+print answer
+
