822d7a566a29933f42d0787e897df365df2ca038
matt-boyd <admin@matt-boyd.co.uk>
2014-01-15 17:26:08 +0000

    Update
    Now asks for each number, and if the check digit is 11, then it will
    come back as 0 and if it is 10, it will come back as X.

--- check_digit.py
+++ check_digit.py
@@ -1,19 +1,24 @@
-one = input()
-two = input()
-three = input()
-four = input()
-five = input()
-six = input()
-seven = input()
-eight = input()
-nine = input()
-ten = input()
+one = input("Please enter your first number: \n")
+two = input("Please enter your second number: \n")
+three = input("Please enter your third number: \n")
+four = input("Please enter your fourth number: \n")
+five = input("Please enter your fifth number: \n")
+six = input("Please enter your sixth number: \n")
+seven = input("Please enter your seventh number: \n")
+eight = input("Please enter your eighth number: \n")
+nine = input("Please enter your ninth number: \n")
+ten = input("Please enter your tenth number: \n")
 
 total = (one * 2) + (two * 3) + (three * 4) + (four * 5) + (five * 6) + (six * 7) + (eight * 9) + (nine * 10) + (ten * 11)
 
 remainder = total % 11
 
 answer = 11 - remainder
+if answer == 11:
+    print "0"
+elif answer == 10:
+    print "X"
 
-print answer
+else:
+    print answer
 
